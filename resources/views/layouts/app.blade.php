c4a255c67cdd774c6b631e956dfc22f0fc736537
Arturr0 <41492101+Arturr0@users.noreply.github.com>
2019-02-01 18:26:31 +0100

    Progress

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Fallen World') }}</title>
+    <title>{{ config('app.name', 'The Old Tales') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
