b632a0d98ea2d462973391908d7358e95caf66e6
Arturr0 <41492101+Arturr0@users.noreply.github.com>
2019-02-10 18:12:26 +0100

    Equipment

--- resources/views/character/equip.blade.php
+++ resources/views/character/equip.blade.php
@@ -25,9 +25,13 @@
         @else
             <a href="{{route("character.equip.part", "body")}}"><div class="flex-item part mx-auto" name="body" id="body"></div></a>
         @endif
-        
-        <a href="{{route("character.equip.part", "legs")}}"><div class="flex-item part mx-auto" name="legs" id="legs">{{$equiped["legs"]}}</div></a> 
-        
+
+        @if(!empty($equiped["legs"]))
+            <a href="{{route("character.equip.part", "legs")}}"><div class="flex-item part mx-auto" name="legs" id="legs"><img src="{{asset('png/' . $equiped["legs"] . '64.png')}}" /></div></a>
+        @else
+            <a href="{{route("character.equip.part", "legs")}}"><div class="flex-item part mx-auto" name="legs" id="legs"></div></a>
+        @endif
+                
         @if(!empty($equiped["foots"]))
             <a href="{{route("character.equip.part", "foots")}}"><div class="flex-item part mx-auto" name="foots" id="foots"><img src="{{asset('png/' . $equiped["foots"] . '64.png')}}" /></div></a>
         @else
