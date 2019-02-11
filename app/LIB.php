94527770aabfaa2032959ca798151289f9a38b29
Arturr0 <41492101+Arturr0@users.noreply.github.com>
2019-02-11 22:33:48 +0100

    Equipment

--- app/LIB.php
+++ app/LIB.php
@@ -9,7 +9,8 @@ class LIB extends Model
     const UTILITIES = 
     [
         'campfire' => ['inside' => [ 'location', 'cottage' ], 'tool' => [], 'building' => ['wood' => 3], 'turns' => 0, 'keeping' => ['wood' => 1], 'storage' => 10, 'places' => 0, 'lifetime' => 6 ],
-        'cottage' => ['inside' => ['location'], 'tool' => ['stone hammer' => 30], 'building' => ['wood' => 10], 'turns' => 3, 'keeping' => [], 'storage' => 30, 'places' => 2, 'lifetime' => -1 ]
+        'cottage' => ['inside' => ['location'], 'tool' => ['stone hammer' => 30], 'building' => ['wood' => 10], 'turns' => 3, 'keeping' => [], 'storage' => 30, 'places' => 2, 'lifetime' => -1 ],
+        'primitiv hut' => ['inside' => ['location'], 'tool' => ['stone hammer' => 30], 'building' => ['wood' => 7], 'turns' => 3, 'keeping' => [], 'storage' => 30, 'places' => 2, 'lifetime' => -1 ]
     ];
 
     static function TOOLS_FOR_RES($res_type)
